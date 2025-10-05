7e2195c99d78be4110e4facaa77a42b9b8594904
Rom05-univalle <ghr0034560@est.univalle.edu>
2025-10-04 22:58:19 -0400

    Scripts Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Text highScoreText;
+
+    void Start()
+    {
+        highScoreText.text = "High Score: " + PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene"); // asegúrate que la escena se llame así
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
